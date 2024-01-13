eb91ab87f80980aea26e34ba430b624b66e8b716
swapno963 <swapno963@gmail.com>
2024-01-14 03:14:15 +0600

    filter done

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.Show_all_task.as_view(), name='show_all_task')    
+    path('',views.priority_filter, name='priority_wise_filter') , 
+
+    path('task/<int:priority>/',views.priority_filter, name='priority_wise_filter') , 
+
+
+    path('/status/<slug:current_status>/',views.status_filter, name='status_wise_filter') , 
+
+    path('/due_date/<slug:due_date>/',views.due_date_filter, name='due_date_filter') , 
+
+
+   
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 from .models import Tasks
@@ -9,6 +11,12 @@ class Show_all_task(ListView):
     template_name = 'show_task.html'
     model = Tasks
     form_class = Task_form
+
+    # for filtering
+    def get_queryset(self):
+        priject_slug = self.kwargs.get('project')
+        # print(priject_slug)
+        return super().get_queryset()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -26,4 +34,53 @@ class Show_all_task(ListView):
             return self.get(request,*args, **kwargs)
 
 
+
+def priority_filter(request, priority=None):
+    form = Task_form()
+    if priority is not None:
+        data = Tasks.objects.filter(priority = priority)
+        print( "slug",priority,'data', data)
+        return render(request,'show_task.html',{'form':form,'object_list':data})
+    if request.method == 'POST':
+        form = Task_form(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            print("from pri :", form.cleaned_data)
+
+    data = Tasks.objects.all()
+    print( "slug",priority,'data', data)
+    return render(request,'show_task.html',{'form':form,'object_list':data})
+
+
+def status_filter(request, current_status=None):
+    form = Task_form()
+    if current_status is not None:
+        data = Tasks.objects.filter(current_status = current_status)
+        print( "slug",current_status,'data', data)
+        return render(request,'show_task.html',{'form':form,'object_list':data})
+    if request.method == 'POST':
+        form = Task_form(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            print("from pri :", form.cleaned_data)
+
+    data = Tasks.objects.all()
+    print( "slug",current_status,'data', data)
+    return render(request,'show_task.html',{'form':form,'object_list':data})
+
+def due_date_filter(request, due_date=None):
+    form = Task_form()
+    if due_date is not None:
+        data = Tasks.objects.filter(due_date = due_date)
+        print( "slug",due_date,'data', data)
+        return render(request,'show_task.html',{'form':form,'object_list':data})
+    if request.method == 'POST':
+        form = Task_form(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            print("from pri :", form.cleaned_data)
+
+    data = Tasks.objects.all()
+    print( "slug",due_date,'data', data)
+    return render(request,'show_task.html',{'form':form,'object_list':data})
 
